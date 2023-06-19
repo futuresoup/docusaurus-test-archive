aca14dbd0e6911ef42bec9429aa3e7ee91da2adc
bigolboyyo <wesley.grant94@gmail.com>
2023-06-19 16:45:00 -0400

    new climate clock first iteration | needs TS typings still

--- src/components/NewClimateClock/NewClimateClock.jsx
+++ src/components/NewClimateClock/NewClimateClock.jsx
@@ -1,37 +1,36 @@
 import React from "react";
 import clsx from "clsx";
-import styles from "./ClimateClock.module.css";
+import styles from "./NewClimateClock.module.css";
 
-export default function ClimateClock({ countDown }) {
+export default function NewClimateClock({ countDown }) {
   return (
     <div id="clock" className={clsx(styles.clock)}>
       <div className={clsx(styles.countdown)}>
         <h3 className={clsx(styles.clockHeader)} id="time-to-two">
           Time Left To Limit Global Warming To 1.5ºC
         </h3>
-        <main className ={clsx(styles.clockMain)}>
-          
+        <main className={clsx(styles.clockMain)}>
           <div className={clsx(styles.section)}>
             <div className={clsx(styles.number)}>{countDown.years}</div>
             <div className="label"> Years</div>
           </div>
 
           <span> : </span>
-          
+
           <div className={clsx(styles.section)}>
             <div className={clsx(styles.number)}>{countDown.months}</div>
             <div className="label">Months</div>
           </div>
 
           <span> : </span>
-          
+
           <div className={clsx(styles.section)}>
             <div className={clsx(styles.number)}>{countDown.days}</div>
             <span className="label">Days</span>
           </div>
 
           <span> : </span>
-          
+
           <div className={clsx(styles.section)}>
             <div className={clsx(styles.number)}>{countDown.hours}</div>
             <span className="label">Hours</span>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import moment from 'moment'
+import React, { useEffect, useState } from "react";
+import moment from "moment";
 import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import HomepageHeader from "../components/HomepageHeader/HomepageHeader";
@@ -7,6 +7,7 @@ import HomeCard from "../components/HomeCard/HomeCard";
 import styles from "./index.module.css";
 import clsx from "clsx";
 import ClimateClock from "../components/ClimateClock/ClimateClock";
+import NewClimateClock from "../components/NewClimateClock/NewClimateClock";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 
 export default function Home(): JSX.Element {
@@ -56,7 +57,8 @@ export default function Home(): JSX.Element {
     >
       <HomepageHeader />
       <main className={clsx(styles.mainBody)}>
-        <ClimateClock/>
+        {/* <ClimateClock /> */}
+        <NewClimateClock countDown={climateCountdown} />
         <HomeCard
           title="Job Seekers"
           description="testing autodeploy dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -71,6 +73,4 @@ export default function Home(): JSX.Element {
       </main>
     </Layout>
   );
-
-  
 }
